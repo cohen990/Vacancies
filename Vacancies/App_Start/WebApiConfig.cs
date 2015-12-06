738f678b16dc146cf879b0bae878a64a31f3a3ad
Dan Cohen <dan.cohen@euromoneyplc.com>
2015-12-06 07:09:14 +0000

    done stuff

---
+++ Vacancies/App_Start/WebApiConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
+
+class WebApiConfig
+{
+    public static void Register(HttpConfiguration configuration)
+    {
+        configuration.Routes.MapHttpRoute("API Default", "api/{controller}/{id}",
+            new { id = RouteParameter.Optional });
+        configuration.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+    }
+}
---
+++ Vacancies/Controllers/EndpointController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Vacancies.Controllers
+{
+    public class EndpointController : ApiController
+    {
+        public static int Num = 1;
+        [System.Web.Http.HttpGet]
+        public int Index()
+        {
+            return (Num);
+        }
+        [System.Web.Http.HttpGet]
+        public void Set(int num)
+        {
+            Num = num;
+            var result = new JsonResult();
+        }
+    }
+}
